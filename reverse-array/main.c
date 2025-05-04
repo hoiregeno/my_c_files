8974f7b195bd322b680834ad5c2978ac1f2ce907
hoiregeno <hoiregeno75@gmail.com>
2025-05-04 13:19:48 +1000

    Add reverseArray and printArray utility functions

--- reverse-array/main.c
+++ reverse-array/main.c
@@ -1,9 +1,50 @@
 #include <stdio.h>
 
+void reverseArray(int *arr, int len);
+void printArray(const int *arr, int len);
+
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5};
     int len = sizeof(arr) / sizeof(arr[0]);
 
+    // before reverse
+    printf("Before:\n");
+    printArray(arr, len);
+
+    // reverse array
+    reverseArray(arr, len);
+
+    // after reverse
+    printf("\n\nAfter:\n");
+    printArray(arr, len);
+
     return 0;
+}
+
+void reverseArray(int *arr, int len)
+{
+    // initialize pointers
+    int *start = arr;         // points to the first element of the array.
+    int *end = arr + len - 1; // points to the last element of the array.
+
+    while (start < end)
+    {
+        // swap elements pointed to by start and end.
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++; // move forward
+        end--;   // move backward
+    }
+}
+
+// Precede int array with const if not mutating anything
+void printArray(const int *arr, int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 }
